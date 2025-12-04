cbce328a6972fd1f8bf2e02bf8048500818ed03e
Nick <nick219999@gmail.com>
2025-12-04 15:18:21 -0500

    commecné patron singleton

--- myriapod-master/systems/world.py
+++ myriapod-master/systems/world.py
@@ -1,2 +1,10 @@
-game = None
-screen = None
+game_instance = None
+
+def game(screen=None):
+  global game_instance
+  if game_instance is None and screen is None:
+    print("error screen has not been initialized")
+  elif game_instance is None:
+    game_instance = Game(screen)
+  else:
+    return game_instance
